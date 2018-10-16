150c84ba41505c573c4fc51da957491cbb0dd239
Creatint <creatint@163.com>
2018-10-16 15:57:17 +0800

    修改了getUser.php返回数据的结构

--- getUser.php
+++ getUser.php
@@ -2,33 +2,33 @@
 include ("config.php");
 
 if (empty($_GET)) {
-	print "ERROR: Wrong usage";
-	die();
+    print "ERROR: Wrong usage";
+    die();
 }
 
 if (!isset($_GET["appcode"])) {
-	print "ERROR: Wrong appcode";
-	die();
+    print "ERROR: Wrong appcode";
+    die();
 }
 
 if ($_GET["appcode"] != $config["appcode"]) {
-	print "ERROR: Wrong appcode";
-	die();
+    print "ERROR: Wrong appcode";
+    die();
 }
 
 if (!isset($_GET["id"]) && !isset($_GET["name"])) {
-	print "ERROR: Wrong usage";
-	die();
+    print "ERROR: Wrong usage";
+    die();
 }
 
 $loadProgression = $config["default-progression"];
 
 if (isset($_GET["progression"])) {
-	$loadProgression = $_GET["progression"];
+    $loadProgression = $_GET["progression"];
 }
 
 if ($loadProgression != "true" && $loadProgression != "false") {
-	$loadProgression = $config["default-progression"];
+    $loadProgression = $config["default-progression"];
 }
 
 include ("UbiAPI.php");
@@ -39,128 +39,131 @@ $region = $config["default-region"];
 $season = -1;
 
 if (isset($_GET['season'])) {
-	$season = $_GET['season'];
+    $season = $_GET['season'];
 }
 
 $platform = $config["default-platform"];
 
 if (isset($_GET['platform'])) {
-	$platform = $_GET['platform'];
+    $platform = $_GET['platform'];
 }
 
 if (isset($_GET['region'])) {
-	$region = $_GET['region'];
+    $region = $_GET['region'];
 }
 
 $notFound = [];
 
 function printName($uid)
 {
-	global $uapi, $data, $id, $platform, $notFound;
-	$su = $uapi->searchUser("byid", $uid, $platform);
-	if ($su["error"] != true) {
-		$data[$su['uid']] = array(
-			"profile_id" => $su['uid'],
-			"nickname" => $su['nick']
-		);
-	} else {
-		$notFound[$uid] = [
-			"profile_id" => $uid,
-			"error" => [
-				"message" => "User not found!"
-			]
-		];
-	}
+    global $uapi, $data, $id, $platform, $notFound;
+    $su = $uapi->searchUser("byid", $uid, $platform);
+    if ($su["error"] != true) {
+        $data[] = array(
+            "id" => $su['uid'],
+            "profile_id" => $su['uid'],
+            "nickname" => $su['nick']
+        );
+    } else {
+        $notFound[] = [
+            'id' => $uid,
+            "profile_id" => $uid,
+            "error" => [
+                "message" => "User not found!"
+            ]
+        ];
+    }
 }
 
 function printID($name)
 {
-	global $uapi, $data, $id, $platform, $notFound;
-	$su = $uapi->searchUser("bynick", $name, $platform);
-	if ($su["error"] != true) {
-		$data[$su['uid']] = array(
-			"profile_id" => $su['uid'],
-			"nickname" => $su['nick']
-		);
-	} else {
-		$notFound[$name] = [
-			"nickname" => $name,
-			"error" => [
-				"message" => "User not found!"
-			]
-		];
-	}
+    global $uapi, $data, $id, $platform, $notFound;
+    $su = $uapi->searchUser("bynick", $name, $platform);
+    if ($su["error"] != true) {
+        $data[] = array(
+            "id" => $su['uid'],
+            "profile_id" => $su['uid'],
+            "nickname" => $su['nick']
+        );
+    } else {
+        $notFound[] = [
+            "nickname" => $name,
+            "error" => [
+                "message" => "User not found!"
+            ]
+        ];
+    }
 }
 
 if (isset($_GET["id"])) {
-	$str = $_GET["id"];
-	if (strpos($str, ',') !== false) {
-		$tocheck = explode(',', $str);
-	}
-	else {
-		$tocheck = array(
-			$str
-		);
-	}
+    $str = $_GET["id"];
+    if (strpos($str, ',') !== false) {
+        $tocheck = explode(',', $str);
+    }
+    else {
+        $tocheck = array(
+            $str
+        );
+    }
 
-	foreach($tocheck as $value) {
-		printName($value);
-	}
+    foreach($tocheck as $value) {
+        printName($value);
+    }
 }
 
 if (isset($_GET["name"])) {
-	$str = $_GET["name"];
-	if (strpos($str, ',') !== false) {
-		$tocheck = explode(',', $str);
-	}
-	else {
-		$tocheck = array(
-			$str
-		);
-	}
+    $str = $_GET["name"];
+    if (strpos($str, ',') !== false) {
+        $tocheck = explode(',', $str);
+    }
+    else {
+        $tocheck = array(
+            $str
+        );
+    }
 
-	foreach($tocheck as $value) {
-		printID($value);
-	}
+    foreach($tocheck as $value) {
+        printID($value);
+    }
 }
 
 if (empty($data)) {
-	$error = $uapi->getErrorMessage();
-	if ($error === false) {
-		die(json_encode(array(
-			"players" => $notFound
-		)));
-	}
-	else {
-		die(json_encode(array(
-			"players" => array() ,
-			"error" => $error
-		)));
-	}
+    $error = $uapi->getErrorMessage();
+    if ($error === false) {
+        die(json_encode(array(
+            "players" => $notFound
+        )));
+    }
+    else {
+        die(json_encode(array(
+            "players" => array() ,
+            "error" => $error
+        )));
+    }
 }
 
 function getValue($user, $progression)
 {
-	foreach($progression as $usera) {
-		if ($usera["profile_id"] == $user) {
-			return $usera;
-		}
-	}
+    foreach($progression as $usera) {
+        if ($usera["profile_id"] == $user) {
+            return $usera;
+        }
+    }
 
-	return array();
+    return array();
 }
 
 $ids = "";
 
 foreach($data as $value) {
-	$ids = $ids . "," . $value["profile_id"];
+    $ids = $ids . "," . $value["profile_id"];
 }
 
 $ids = substr($ids, 1);
 $idresponse = json_decode($uapi->getRanking($ids, $season, $region, $platform) , true);
 
 if ($loadProgression == "true") {
-	$progression = json_decode($uapi->getProgression($ids, $platform) , true) ["player_profiles"];
+    $progression = json_decode($uapi->getProgression($ids, $platform) , true) ["player_profiles"];
 }
 
 $ranks = json_decode('{
@@ -252,15 +255,24 @@ $ranks = json_decode('{
 $final = array();
 
 foreach($idresponse["players"] as $value) {
-	$id = $value["profile_id"];
-	$final[$id] = array_merge(($loadProgression == "true" ? getValue($id, $progression) : array()) , $value, array(
-		"nickname" => $data[$id]["nickname"],
-		"platform" => $platform,
-		"rankInfo" => $ranks[$value["rank"]]
-	));
+    $id = $value["profile_id"];
+    $final[] = array_merge(($loadProgression == "true" ? getValue($id, $progression) : array()) , $value, array(
+        "id" => $id,
+        "nickname" => $data[$id]["nickname"],
+        "platform" => $platform,
+        "rankInfo" => $ranks[$value["rank"]]
+    ));
 }
 
-print json_encode(array(
-	"players" => array_merge($final, $notFound)
-));
+if (empty($notFound)) {
+    print json_encode(array(
+        'code' => 0,
+        "data" => $final
+    ));
+} else {
+    print json_encode(array(
+        'code' => -1,
+        "data" => array_merge($final, $notFound)
+    ));
+}
 ?>
